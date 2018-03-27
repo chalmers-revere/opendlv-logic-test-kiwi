32671ca5bad00a1097b5fd50b71d29252029b2e5
olbender <olbender@users.noreply.github.com>
2018-03-27 02:35:51 +0200

    Minor change

--- src/behavior.hpp
+++ src/behavior.hpp
@@ -15,8 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef WORLD_OBJECT
-#define WORLD_OBJECT
+#ifndef BEHAVIOR
+#define BEHAVIOR
 
 #include <mutex>
 
